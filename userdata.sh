366e4e095678952a5d3b2a7e733310e55f7d2a49
sumitgupta190a <84587186+sumitgupta190a@users.noreply.github.com>
2023-09-28 23:40:31 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -32,7 +32,7 @@ cat <<EOF > /var/www/html/index.html
 <body>
   <h1>Terraform Project Server 1</h1>
   <h2>Instance ID: <span style="color:green">$INSTANCE_ID</span></h2>
-  <p>This is terraform project make by Sumit Gupta and this is WEBPAGE 1</p>
+  <p>A Terraform project created by Sumit Gupta. This is WEBPAGE 1</p>
   
 </body>
 </html>
